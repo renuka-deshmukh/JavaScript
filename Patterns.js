889b5be2fb5bab59ef03b5aa89457a871de852d0
renuka-deshmukh <renuka.deshmukh776@gmail.com>
2025-08-18 15:36:31 +0530

    updated

--- Patterns.js
+++ Patterns.js
@@ -1,51 +1,169 @@
 
-n = 5;
-
-for(let i=1; i<=n; i++){
-    line = '';
-    for(let j = 1; j <=n; j++){
-        line += " *"
+// square
+function printSquare(n = 5) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+      row += "* ";
     }
-    console.log(line)
-}
-  console.log("\n")
-
-for(let i = 1; i<=n; i++){
-    line = '';
-    for(let j= 1;j<=i; j++){
-        line += " *"
-    }
-    console.log(line)
-}
-
-for(i = 5; i>=1; i--){
-    line = '';
-    for(j=1; j<=i; j++){
-        line += " *"
-    }
-    console.log(line)
-}
-
-console.log("\n")
-
-//     *
-//    * *
-//   * * *
-//  * * * *
-// * * * * *
-
-for(i = 1 ; i<=n; i++){
-  line = ''
-
-  for(j= 1; j <= n-i; j++){
-    line+= ' '
+    console.log(row);
   }
-  for(k =1; k<=i; k++){
-    line +=' *'
+}
+
+printSquare();
+
+//Right angle triangle
+function printTriangle(height = 5) {
+  for (let i = 1; i <= height; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "* ";
+    }
+    console.log(row);
   }
-  for(k =1; k<=i; k++){
-    line +=' *'
+}
+printTriangle();
+
+function printInvertedTriangle(height = 5) {
+  for (let i = height; i >= 1; i--) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "* ";
+    }
+    console.log(row);
+  }
+}
+
+printInvertedTriangle();
+
+
+function printDiamond(height = 5) {
+
+  for (let i = 1; i <= height; i++) {
+    let row = " ".repeat(height - i) + "* ".repeat(i);
+    console.log(row);
   }
 
-  console.log(line)
+
+  for (let i = height - 1; i >= 1; i--) {
+    let row = " ".repeat(height - i) + "* ".repeat(i);
+    console.log(row);
+  }
 }
+
+
+printDiamond();
+
+
+function printInvertedTriangle(height = 5) {
+  for (let i = height; i >= 1; i--) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "* ";
+    }
+    console.log(row);
+  }
+}
+
+printInvertedTriangle();
+
+
+function printDiamond(height = 5) {
+
+  for (let i = 1; i <= height; i++) {
+    let row = " ".repeat(height - i) + "* ".repeat(i);
+    console.log(row);
+  }
+
+  for (let i = height - 1; i >= 1; i--) {
+    let row = " ".repeat(height - i) + "* ".repeat(i);
+    console.log(row);
+  }
+}
+
+printDiamond();
+
+function printPyramid(n = 5) {
+  for (let i = 1; i <= n; i++) {
+    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
+}
+
+printPyramid();
+
+function printNumberPyramid(n = 5) {
+  for (let i = 1; i <= n; i++) {
+    console.log(" ".repeat(n - i) + (i.toString()).repeat(2 * i - 1));
+  }
+}
+
+// Call the function
+printNumberPyramid();
+
+function printPascalsTriangle(n = 5) {
+  for (let i = 0; i < n; i++) {
+    let row = " ".repeat(n - i);
+    let value = 1;
+    for (let j = 0; j <= i; j++) {
+      row += value + " ";
+      value = value * (i - j) / (j + 1);
+    }
+    console.log(row);
+  }
+}
+
+printPascalsTriangle();
+
+function printHollowSquare(n = 5) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+
+      if (i === 0 || i === n - 1 || j === 0 || j === n - 1) {
+        row += "* ";
+      } else {
+        row += "  ";
+      }
+    }
+    console.log(row);
+  }
+}
+
+printHollowSquare();
+
+function printHourglass(n = 4) {
+
+  for (let i = n; i >= 1; i--) {
+    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
+
+  for (let i = 2; i <= n; i++) {
+    console.log(" ".repeat(n - i) + "*".repeat(2 * i - 1));
+  }
+}
+
+
+printHourglass();
+
+function printZigzag(n = 3, length = 10) {
+
+  let grid = Array.from({ length: n }, () => Array(length).fill(" "));
+
+  let row = 0, down = true;
+  for (let col = 0; col < length; col++) {
+    grid[row][col] = "*";
+
+    if (down) {
+      row++;
+      if (row === n - 1) down = false;
+    } else {
+      row--;
+      if (row === 0) down = true;
+    }
+  }
+
+
+  grid.forEach(r => console.log(r.join("")));
+}
+
+printZigzag(3, 15);
